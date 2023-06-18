19d4914ae8dc066359998d635e9bd51ace784fb1
Eric Nordström <eric@nrdstrm.org>
2023-06-18 22:12:00 +0200

    dot.sh: Install tmux plugins during setup

--- dot.sh
+++ dot.sh
@@ -3,9 +3,18 @@
 DOT_ORIGIN_URL="${DOT_ORIGIN:-https://github.com/enordstrm/dotfiles}"
 DOT_GIT_DIR="${DOT_GIT_DIR:-${HOME}/.dot.git}"
 DOT_BACKUP_DIR="${DOT_BACKUP_DIR:-${HOME}/.dot.bak}"
+DOT_TMUX_PLUGIN_INSTALL_SCRIPT="${TMUX_PLUGIN_INSTALL_SCRIPT:-${HOME}/.tmux/plugins/tpm/scripts/install_plugins.sh}"
 
-dot() {
+dot::git() {
     git --git-dir="${DOT_GIT_DIR}" --work-tree="${HOME}" "$@"
+}
+
+dot::tmux_plugin_install() {
+    if [ -f "${DOT_TMUX_PLUGIN_INSTALL_SCRIPT}" ]; then
+        ${DOT_TMUX_PLUGIN_INSTALL_SCRIPT}
+    else
+        echo "Warning: ${DOT_TMUX_PLUGIN_INSTALL_SCRIPT} not found!"
+    fi
 }
 
 main() {
@@ -17,20 +26,23 @@ main() {
     # not set by default for bare repositories.
     #dot config remote.origin.fetch "+refs/heads/*;refs/remotes/origin/*"
 
-    if ! dot checkout; then
+    if ! dot::git checkout; then
         echo -n "Files exist, making backup... "
         mkdir -p "${DOT_BACKUP_DIR}"
-        dot checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} \
+        dot::git checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} \
             "${DOT_BACKUP_DIR}/{}"
         echo "done"
 
-        dot checkout
+        dot::git checkout
     fi
 
-    dot submodule update --init --recursive
+    dot::git submodule update --init --recursive
 
     # Hide untracked files since work tree is our home directly (by default).
-    dot config status.showUntrachedFiles no
+    dot::git config status.showUntrachedFiles no
+
+    # Install TMUX plugins
+    dot::tmux_plugin_install
 }
 
 main "${@}"
